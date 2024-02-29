c9f9eab152de458678836dd5381b1737223e7370
Bae Junho <58718078+Juno-hub@users.noreply.github.com>
2024-02-29 23:53:58 +0900

    Making calendar

--- twenty-salon.py
+++ twenty-salon.py
@@ -1,4 +1,6 @@
 from tkinter import *
+from openpyxl import Workbook
+from datetime import datetime, timedelta
 
 def set_screen():
     # Set screen icon
@@ -24,21 +26,49 @@ def set_screen():
     root.geometry(f"{app_width}x{app_height}+{x}+{y}")
 
 def add_name():
-    name = entry_name.get().strip()  # 이름 입력 받음
+    name = entry_name.get().strip() # 이름 입력 받음
     if name:
-        list_name.insert(END, name)  # 이름 리스트에 추가
-        entry_name.delete(0, END)  # 이름 입력 텍스트 상자 초기화
+        list_name.insert(END, name) # 이름 리스트에 추가
+        entry_name.delete(0, END)   # 이름 입력 텍스트 상자 초기화
     else:
-        entry_name.insert(0, "이름")  # 이름이 비어있을 경우 기본값으로 설정
+        entry_name.insert(0, "이름") # 이름이 비어있을 경우 기본값으로 설정
 
 def delete_name():
     list_name.delete(list_name.curselection()) # 선택된 리스트를 삭제
 
 def clear_name():
     list_name.delete(0, END) # Listbox 에 있는 멤버 명단 초기화
+    
 
-def add_schedule():
-    pass
+def add_schedule(year, month):
+    # Create a new Workbook
+    wb = Workbook()
+    ws = wb.active
+
+    # Set properties of WorkSheet
+    ws.title = "스무살롱 녹음일정"
+    ws.sheet_properties.tabColor = "0888D0" # RGB 형태로 값을 넣어주면 탭 색상 변경
+
+    # Define weekdays
+    weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+    
+    # Set column headers with weekdays
+    for i, day in enumerate(weekdays):
+        ws.cell(row=1, column=i+1, value=day)
+    
+    # Get the first day of the month
+    first_day = datetime(year, month, 1)
+
+    # Calculate the starting column for the first day
+    start_column = first_day.weekday() + 1
+    print(first_day.weekday())
+    # # 가용 가능한 인원을 기록하는 WorkSheet
+    # ws_available = wb.create_sheet("3월 녹음 가용 인원")
+    # member = list_name.get(0, END) # 가용 가능 인원
+    # ws_available.append(member)
+
+    wb.save("스무살롱_녹음일정.xlsx")
+    wb.close()
 
 # Create root window
 root = Tk()
@@ -70,6 +100,11 @@ button_delete.grid(row=1, column=2, padx=3, pady=3)
 # 이름 초기화 버튼
 button_clear = Button(frame_member, text="초기화", command=clear_name)
 button_clear.grid(row=2, column=0, columnspan=3, sticky=N+E+W+S, padx=3, pady=3)
+
+# # 일정을 추가하려는 시작 회차 입력을 위한 텍스트 상자
+# entry_episode = Entry(root, width=10)
+# entry_episode.insert(0, "시작 회차")
+# entry_episode.grid(row=)
 
 # Set frame to manage number of member
 frame_number_member = LabelFrame(root, text="녹음 인원 설정")
